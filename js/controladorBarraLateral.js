1ac37962d401f4836e03471ceae409b924f69bd6
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2023-12-17 17:30:59 -0300

    a

--- js/controladorBarraLateral.js
+++ js/controladorBarraLateral.js
@@ -1,15 +1,18 @@
 document.getElementById('toggleButton').addEventListener('click', function () {
     var menuLateral = document.querySelector('.menu-lateral');
     // var modoEscuro = document.querySelector('.modo-escuro')
+
     if(menuLateral.classList.contains("expandido")){
-        menuLateral.classList.remove('expandido')
-        // modoEscuro.classList.remove('expandido')
+        menuLateral.classList.remove('expandido');
         menuLateral.classList.toggle('retraido');
+
+        // modoEscuro.classList.remove('expandido')
         // modoEscuro.classList.toggle('retraido');
     }else{
-        menuLateral.classList.remove('retraido')
-        // modoEscuro.classList.remove('retraido')
+        menuLateral.classList.remove('retraido');
         menuLateral.classList.toggle('expandido');
+
+        // modoEscuro.classList.remove('retraido')
         // modoEscuro.classList.toggle('expandido');
     }
 });
